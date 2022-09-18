729c124d2f6794c4292e9fe76761eff2672cbe0d
Jeff Becker <jeff@lokinet.io>
2022-09-17 20:13:31 -0400

    fixup makefile and helper to build properly

--- lokinet/contrib/build-docker-images.sh
+++ lokinet/contrib/build-docker-images.sh
@@ -7,6 +7,6 @@ test "x$registry" != "x" || exit 1
 
 for file in ${@:2} ; do
     name="$(echo $file | cut -d'.' -f1)"
-    docker build -f $file -t $registry/$name  .
-    docker push $registry/$name
+    docker build -f $file -t $registry/$name:latest  .
+    docker push $registry/$name:latest
 done
